e145267d2e7b5b95f53100c71099f5b3aea45146
idadidut <muhammad.iddad@gmail.com>
2019-09-21 20:50:15 +0700

    init

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # modred
 Learn Rest API with Django
+
+basic knowledge I adopt from https://medium.com/@djstein/modern-django-part-0-introduction-and-initial-setup-657df48f08f8 and https://www.django-rest-framework.org/
---
+++ oberon/api/views.py
@@ -0,0 +1,12 @@
+from django.shortcuts import render
+from django.contrib.auth.models import User
+from rest_framework import viewsets
+from oberon.api.serializers import UserSerializer
+
+# Create your views here.
+class UserViewSet(viewsets.ModelViewSet):
+  """
+  API endpoint that allows users to be viewed or edited.
+  """
+  queryset = User.objects.all()
+  serializer_class = UserSerializer
